f3e6d4913b5fba6e7477f6479c46af14c4e2e3bd
meilstrupta <mei21001@byui.edu>
2026-01-21 12:54:25 -0700

    Updated Prep Programs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello! What is your first name?");
+        string first_name = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Hello, your name is {last_name}, {first_name} {last_name}!");
+        Console.WriteLine("Thank You!");
+
     }
 }
